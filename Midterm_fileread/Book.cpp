1921d966f39e07b17ae8843e0f8cd3695f0287a8
Hunter Zhong <hunterzhong929@gmail.com>
2022-11-05 10:45:32 -0400

    added stream operator and getter setter

--- Midterm_fileread/Book.cpp
+++ Midterm_fileread/Book.cpp
@@ -24,7 +24,17 @@ Book::Book(string isbn, string t, string a, string c) {
 	int expirationDate = 0;
 }
 
-string Book::getISBN() {
-	return ISBN;
+
+
+std::ostream &operator<<(std::ostream &os, const Book &rhs) {
+	os << "ISBN: " << rhs.ISBN
+		<< " title: " << rhs.title
+		<< " author: " << rhs.author
+		<< " category: " << rhs.category
+		<< " ID: " << rhs.ID
+		<< " readerName: " << rhs.readerName
+		<< " startDate: " << rhs.startDate
+		<< " expirationDate: " << rhs.expirationDate;
+	return os;
 }
 
--- Midterm_fileread/Book.h
+++ Midterm_fileread/Book.h
@@ -16,9 +16,21 @@ private:
 public:
 	Book();
 	Book(string isbn, string t, string a, string c);
-	string getISBN();
-	string getTitle();
-	string getAuthor();
+	string iSBN() const { return ISBN; }
+	void setISBN(const string &iSBN) { ISBN = iSBN; }
+	string getTitle() const { return title; }
+	void setTitle(const string &title_) { title = title_; }
+	string getAuthor() const { return author; }
+	void setAuthor(const string &author_) { author = author_; }
+	string getCategory() const { return category; }
+	void setCategory(const string &category_) { category = category_; }
+	int iD() const { return ID; }
+	void setID(int iD) { ID = iD; }
+	string getReaderName() const { return readerName; }
+	void setReaderName(const string &readerName_) { readerName = readerName_; }
+	int getStartDate() const { return startDate; }
+	void setStartDate(int startDate_) { startDate = startDate_; }
+	friend std::ostream &operator<<(std::ostream &os, const Book &rhs);
 
 	//overload >> to print all info
 };
